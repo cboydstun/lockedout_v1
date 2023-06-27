dd7f180e3896c306f387c1cca32112bd3a562b9f
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-06-27 02:58:48 -0500

    added headers and favicons

--- renderer/_default.page.server.jsx
+++ renderer/_default.page.server.jsx
@@ -27,10 +27,17 @@ async function render(pageContext) {
         <meta name="robots" content="index, follow">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+        <title>24 Hour Locksmith San Antonio TX | Affordable Emergency Services</title>
+        <meta name="description" content="Are you locked out of your car or home? Our 24 hour locksmith San Antonio services are ready to assist. We also offer affordable and reliable key making and mobile locksmith services in San Antonio, TX.">
+        <meta name="keywords" content="Locksmith San Antonio, Locksmith San Antonio TX, Cheap Locksmith San Antonio TX, Emergency Locksmith San Antonio, Car Locksmith San Antonio TX, 24 Hour Locksmith San Antonio, Key Maker San Antonio, Locksmith Near Me San Antonio, Mobile Locksmith San Antonio">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="canonical" href="https://www.sanantoniolockedout.com/"/>
+
         <link rel="manifest" href="site.webmanifest">
         <link rel="sitemap" type="application/xml" href="sitemap.xml" />
         ${headWithGtagHtml}
-        <title>LOCKED OUT</title>
       </head>
       <body>
         <div id="page-view">${viewHtml}</div>
